3e1772ab6d3d9c97281e415ab92b8964c2366151
UMassGameDevClub <Umassgamedevclub@gmail.com>
2023-10-17 21:39:39 -0400

    Implemented Virtual Camera

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,9 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float moveSpeed = 0.02f;
-    public float jumpHeight = 0.5f;
     public float jumpForce = 10.0f;
     public float linearDrag = 1.0f;
+    public float groundedRaycastLength = 1.8f;
 
     public LayerMask groundLayer;
     private bool grounded = true;
@@ -34,18 +34,14 @@ public class PlayerMovement : MonoBehaviour
         }
         
         // the box sprite is about 1.0f high, so I set the length of the ray to 0.8f since it starts from the center
-        grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, groundLayer).collider != null;
+        grounded = Physics2D.Raycast(transform.position, Vector2.down, groundedRaycastLength, groundLayer).collider != null;
 
         if (Input.GetKey("w") || Input.GetKey("space"))
         {
-            // this is a bad system for jumping that should be replaced with something better
-            // ^ I gotchu
-            // transform.position += new Vector3(0.0f,jumpHeight,0.0f);
             if (grounded)
             {
                 rb.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
             }
-            
         }
     }
 }
