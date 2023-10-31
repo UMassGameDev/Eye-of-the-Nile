48f366d73f41b5e63fa5cb1befbaed1fd9fbad88
roihp <roihpscl60206@gmail.com>
2023-10-31 10:26:49 -0400

    Added basic enemy chasing

--- Assets/Scripts/EntityController.cs
+++ Assets/Scripts/EntityController.cs
@@ -6,15 +6,18 @@ public class EntityController : MonoBehaviour
 {
     ObjectHealth objectHealth;
 
+    // This LayerMask includes the Player's layer so the enemy is alerted
+    public LayerMask enemyLayers;
     public PatrolZone patrolZone;
     public EntityState EState { get; set; } = EntityState.Patrol;
     public float horizontalDirection = 0f;
+    public float detectionRange = 5f;
+    bool hostileInRange = false;
 
     Rigidbody2D rb;
-    public float moveVelocity = 8.0f;
+    public float moveVelocity = 6.0f;
     public float linearDrag = 1.0f;
     public float groundedRaycastLength = 1.8f;
-    bool hostileInRange = false;
 
     Animator animator;
 
@@ -23,25 +26,36 @@ public class EntityController : MonoBehaviour
         if (horizontalDirection == 0f)
             horizontalDirection = 1f;
 
-        animator.SetBool("IsMoving", horizontalDirection != 0f);
-
-        rb.velocity = new Vector2(moveVelocity * horizontalDirection, rb.velocity.y);
-
+        // If to the left of the left point, go right
+        // If to the right of the right point, go left
         if (transform.position.x <= patrolZone.LeftPoint().x)
         {
             horizontalDirection = 1f;
-            transform.localScale = new Vector3(-2,
-                transform.localScale.y,
-                transform.localScale.z);
         }
         else if (transform.position.x >= patrolZone.RightPoint().x)
         {
             horizontalDirection = -1f;
-            transform.localScale = new Vector3(2,
-                transform.localScale.y,
-                transform.localScale.z);
         }
 
+        // Flip the direction of the enemy depending on direction
+        // Facing right = -2 scale for x
+        // Facing left = 2 scale for x
+        if (horizontalDirection != 0f)
+            transform.localScale = new Vector3(-2 * horizontalDirection,
+                    transform.localScale.y,
+                    transform.localScale.z);
+
+        animator.SetBool("IsMoving", horizontalDirection != 0f);
+
+        rb.velocity = new Vector2(moveVelocity * horizontalDirection, rb.velocity.y);
+
+        Collider2D hitObject = Physics2D.OverlapCircle(transform.position,
+            detectionRange,
+            enemyLayers);
+
+        hostileInRange = hitObject != null;
+
+        // Prioritize death
         if (objectHealth.IsDead)
         {
             EState = EntityState.Dead;
@@ -53,9 +67,46 @@ public class EntityController : MonoBehaviour
 
     void Chase()
     {
+        Collider2D hitObject = Physics2D.OverlapCircle(transform.position,
+            detectionRange,
+            enemyLayers);
 
+        hostileInRange = hitObject != null;
 
-        if (!hostileInRange)
+        // If detected enemy is to the left, go left
+        // If detected enemy is to the right, go right
+        if (hostileInRange && hitObject.transform.position.x < transform.position.x)
+        {
+            horizontalDirection = -1f;
+        }
+        else if (hostileInRange && hitObject.transform.position.x > transform.position.x)
+        {
+            horizontalDirection = 1f;
+        }
+        else
+        {
+            horizontalDirection = 0f;
+        }
+
+        // Flip the direction of the enemy depending on direction
+        // Facing right = -2 scale for x
+        // Facing left = 2 scale for x
+        if (horizontalDirection != 0f)
+            transform.localScale = new Vector3(-2 * horizontalDirection,
+                    transform.localScale.y,
+                    transform.localScale.z);
+
+        animator.SetBool("IsMoving", horizontalDirection != 0f);
+
+        rb.velocity = new Vector2(moveVelocity * horizontalDirection, rb.velocity.y);
+
+        // Prioritize death
+        if (objectHealth.IsDead)
+        {
+            EState = EntityState.Dead;
+            horizontalDirection = 0f;
+        }
+        else if (!hostileInRange)
             EState = EntityState.Patrol;
     }
 
