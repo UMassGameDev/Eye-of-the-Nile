b0fc9901b85db2594d827c4d5f84f56a5590f10e
Art126 <artalex126@gmail.com>
2024-11-26 22:34:31 -0500

    Fixed Wall Jump Bug
    - Fixed wall jump bug: There was a bug where you could sometimes do wall jumps if you slammed yourself into a wall, which would trigger the ground detector for a few frames, resetting the player's jump. This was fixed by introducing a delay where isGrounded doesn't get set to true until a few frames after the entity touches the ground. The downside to this is that there is now a frame or two delay before the player can jump after touching the ground.
    - The size of the ground detector trigger zone has been adjusted.

--- Assets/Scripts/Player/GroundDetector.cs
+++ Assets/Scripts/Player/GroundDetector.cs
@@ -8,41 +8,56 @@ NOTE: This detects the layer that is selected on this script in the inspector, w
 The selected layer should usually be set to "Collision"
 This script can only detect objects that have colliders.
 
-Documentation updated 11/23/2024
+Documentation updated 11/26/2024
 \author Stephen Nuttall, Alexander Art
 */
 public class GroundDetector : MonoBehaviour
 {
     /// The layers which objects are considered part of the ground are on.
     public LayerMask groundLayer;
-    /// True if the player touching the ground.
+    /// True if the entity is touching the ground (after a small delay).
     public bool isGrounded {get; private set;} = false;
+    /// Counts how long the entity has been on the ground.
+    float groundTime = 0.0f;
+    /// How long the entity needs to be on the ground before isGrounded is set to true.
+    float isGroundedDelay = 0.025f;
 
     /// <summary>
-    /// Runs when an object enters the ground detector zone. If that object is on the groundLayer, set isGrounded to true.
+    /// This part does nothing now, but it is kept for future reference if ever needed.
     /// </summary>
     /// <param name="col">Represents the object inside the trigger zone.</param>
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if ((1 << col.gameObject.layer & groundLayer.value) != 0)
-            isGrounded = true;
-    }
+    // void OnTriggerEnter2D(Collider2D col)s
+    // {
+    //     if ((1 << col.gameObject.layer & groundLayer.value) != 0) {isGrounded = true;}
+    // }
+
     /// <summary>
-    /// /// This part is not necessary for the groundDetector to work, but is kept for redundancy. Runs when an object is inside the ground detector zone. If that object is on the groundLayer, set isGrounded to true.
+    /// Runs when an object is inside the ground detector zone (Warning: slightly innacurate). If that object is on the groundLayer, increment groundTime.
+    /// If groundTime is greater than the delay required to set isGrounded to true, then set isGrounded to true.
     /// </summary>
     /// <param name="col">Represents the object inside the trigger zone.</param>
     void OnTriggerStay2D(Collider2D col)
     {
         if ((1 << col.gameObject.layer & groundLayer.value) != 0)
+        {
+            groundTime += Time.deltaTime;
+        }
+        if (groundTime > isGroundedDelay)
+        {
             isGrounded = true;
+        }
     }
+
     /// <summary>
-    /// Runs when an object exits the ground detector zone. If that object is on the groundLayer, set isGrounded to false.
+    /// Runs when an object exits the ground detector zone. If that object is on the groundLayer, reset groundTime and set isGrounded to false.
     /// </summary>
     /// <param name="col">Represents the object inside the trigger zone.</param>
     void OnTriggerExit2D(Collider2D col)
     {
         if ((1 << col.gameObject.layer & groundLayer.value) != 0)
+        {
+            groundTime = 0.0f;
             isGrounded = false;
+        }
     }
 }
