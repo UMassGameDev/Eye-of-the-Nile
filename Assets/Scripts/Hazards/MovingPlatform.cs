7f641ca78a51f5efdb1777058332139a0a8e21e6
Art126 <artalex126@gmail.com>
2024-12-23 09:07:56 -0500

    Moving Platform Improvement
    - Redid the moving platform code so now it can move between any number of points, not just two.
    - Fixed the joint wonkiness in the spikeball trap that I added yesterday. It just needed to be set up better. Still, occasionally the chain can get stuck in the wall.

--- Assets/Scripts/Hazards/MovingPlatform.cs
+++ Assets/Scripts/Hazards/MovingPlatform.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /** \brief
 Moves the moving platforms.
 Each moving platform has its own MovingPlatform script attached.
 
-Documentation updated 12/17/2024
+Documentation updated 12/23/2024
 \author Alexander Art
 \todo Prevent entities from clipping into walls when being pushed into them.
 \todo Prevent colliding moving platforms from sticking together (or just never place them in each other's path).
@@ -16,74 +14,57 @@ public class MovingPlatform : MonoBehaviour
 {
     /// Reference to the platform's rigidbody component.
     Rigidbody2D rb;
-    /// A patrol zone is an object that has two points the platform will move between.
-    [SerializeField] protected PatrolZone patrolZone;
-    /// In a different script, this detects the entities that are standing on the platform.
+    /// Detects the entities that are standing on the platform (controlled by a different script).
     [SerializeField] protected PlatformEntityDetector platformEntityDetector;
+    /// The object with children as the points that the platform will move between.
+    [SerializeField] protected Transform path;
+    
     /// The speed at which the platform will move at.
-    public float speed = 2.5f;
-    /// The direction that the platform is moving towards. -1 for left, 1 for right.
-    /// Setting this to anything else will make the platform forget where it moved last.
-    private float direction = -1f;
-    /// The amount of time that the platform will stop before changing directions.
-    public float waitDelay = 1f;
-    /// Keeps track of how long the platform has been stopped before changing directions.
-    /// This is set to 0 when inactive, and will be equal to waitDelay once completed.
-    private float delayCounter = 0f;
+    public float moveSpeed = 2.5f;
+    /// The amount of time the platform will stop between each point (in seconds).
+    public float pauseDuration = 1f;
+
+    /// Keeps track of how long the platform has been stopped before going to a new point (in seconds).
+    private float pauseCounter = 0f;
+    /// The point that the platform should be moving towards.
+    private Transform targetPoint;
+    /// Keeps track of which point the platform should be moving towards.
+    private int targetPointIndex = 0;
     /// Needed for calculating how far the platform has moved each frame when updating the position of entities standing atop.
     private Vector2 previousPosition;
 
-    /// Set reference to the moving platform's rigidbody.
+    /// Set reference to the moving platform's rigidbody and initial target point.
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        targetPoint = path.GetChild(targetPointIndex);
     }
 
     void Update()
     {
-        // If the stop timer has been started, then increment the timer.
-        if (delayCounter != 0f)
-        {
-            delayCounter += Time.deltaTime;
-        }
-
-        // If the platform is done moving, change the direction and start the timer.
-        if (direction == -1f && (Vector2)transform.position == patrolZone.LeftPoint()) 
-        {
-            direction = 1f;
-            delayCounter += Time.deltaTime;
-        }
-        else if (direction == 1f && (Vector2)transform.position == patrolZone.RightPoint())
-        {
-            direction = -1f;
-            delayCounter += Time.deltaTime;
-        }
-
-        // End the timer once enough time has passed.
-        if (delayCounter >= waitDelay)
-        {
-            delayCounter = 0f;
-        }
-
         // Keep track of where the platform was before it gets moved.
         previousPosition = transform.position;
 
-        // If the timer is over, move the platform.
-        if (direction == 1f && delayCounter == 0f)
+        // If the platform has reached its target destination, wait for pauseDuration, then start moving to the next point.
+        if (transform.position == targetPoint.position)
         {
-            delayCounter = 0f;
-            transform.position = Vector2.MoveTowards(transform.position, patrolZone.RightPoint(), speed * Time.deltaTime);
-        }
-        else if (direction == -1f && delayCounter == 0f)
-        {
-            delayCounter = 0f;
-            transform.position = Vector2.MoveTowards(transform.position, patrolZone.LeftPoint(), speed * Time.deltaTime);
+            pauseCounter = pauseCounter + Time.deltaTime;
+            if (pauseCounter >= pauseDuration)
+            {
+                pauseCounter = 0f;
+                targetPointIndex++;
+                targetPointIndex = targetPointIndex % path.childCount; // Loops between points.
+                targetPoint = path.GetChild(targetPointIndex);
+            }
         }
 
-        // Move the entities that stand on the platform by the amount that the platform has moved.
+        // Move the platform.
+        transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+
+        // Move all entities that stand on the platform by the same amount that the platform moved.
         foreach (GameObject entity in platformEntityDetector.entitiesInDetector)
         {
-            // Position keeps wanting to be a Vector3 so that's what's with all the typecasts.
+            // Typecasts are used because position keeps being Vector2 or Vector3.
             entity.transform.position += (Vector3) ((Vector2)transform.position - previousPosition);
         }
     }
