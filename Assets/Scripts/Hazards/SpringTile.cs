6de5b4ef6afd498d7676cf8396d1f4fa74b0e52b
Art126 <artalex126@gmail.com>
2024-12-29 23:51:28 -0500

    Spring Tile Improvements
    - The spring tile support texture (the part that springs up and down) now scales based on the spring tile's current height. Before this change, it didn't scale at all, which didn't matter when you couldn't see below it, but that meant it had to be partially hidden. Now, the spring tile can be placed anywhere! Though, it still looks like a yellow blob since it has no official texture yet.
    - Increased the frequency of the Spring Joint 2D on the spring tile, and suddenly, it's no longer droopy! It looks more springy and the distance on the Spring Joint 2D now directly controls how tall the spring tile will be, rather than it always being 1 shorter than it seems like it should be.

--- Assets/Scripts/Hazards/SpringTile.cs
+++ Assets/Scripts/Hazards/SpringTile.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 Moves entities when they jump on a spring tile.
 Change the spring's mass (and other properties) to change how bouncy the spring appears.
 To adjust the height of the spring tile, place it 1 tile above the floor, then set the "Distance"
-on the Spring Joint 2D to be 1 greater than where you would like the spring to end up.
+on the Spring Joint 2D to be where you would like the spring to end up.
 
-Documentation updated 12/25/2024
+Documentation updated 12/29/2024
 \author Alexander Art
 \todo Make the amount that the spring launches the entities based on the mass of the entity and how compressed the spring is.
 \todo Give the spring itself some positive velocity when launching.
 \todo Prevent the spring from getting stuck in the floor.
-\todo Make the support beam for the spring tile extend and retract as the spring tile moves up and down, rather than having it always be as long as possibly necessary.
 */
 public class SpringTile : MonoBehaviour
 {
@@ -19,6 +18,10 @@ public class SpringTile : MonoBehaviour
     Rigidbody2D rb;
     /// Detects the entities that are standing on the platform (controlled by a different script).
     [SerializeField] protected MovableEntityDetector entityDetector;
+    /// Object of the texture under the spring tile that makes it visually connect to the ground.
+    [SerializeField] protected GameObject supportTexture;
+    /// The point under the spring tile that the Spring Joint 2D connects to.
+    [SerializeField] protected GameObject anchor;
     
     /// The speed at which the spring tile will launch entities at.
     public float launchSpeed = 50f;
@@ -35,9 +38,13 @@ public class SpringTile : MonoBehaviour
     /// <summary>
     /// If the spring was not compressed, but an entity is on the spring and the spring is moving downwards, then it is compressed.
     /// If the spring is compressed, wait for it to stop compressing, then launch the spring.
+    /// 
+    /// Additionally, scale the height of the support texture to match the height of the spring.
+    /// The height is precisely the difference between the anchor point and the bottom of the platform.
     /// </summary>
     void Update()
     {
+        // Handle compression.
         if (compressed == false && entityDetector.entitiesInDetector.Count > 0 && rb.velocity.y < 0)
         {
             compressed = true;
@@ -46,7 +53,14 @@ public class SpringTile : MonoBehaviour
         {
             LaunchSpring();
             compressed = false;
-        }        
+        }
+
+        // Change the height (y scale) of the support texture.
+        float targetHeight = (transform.position.y - anchor.transform.position.y - transform.parent.localScale.y * transform.localScale.y / 2) / transform.localScale.y / transform.parent.localScale.y;
+        supportTexture.transform.localScale = new Vector2(supportTexture.transform.localScale.x, targetHeight);
+        // Adjust the position of the support texture to match the new height.
+        float targetPosition = -0.5f - targetHeight / 2;
+        supportTexture.transform.localPosition = new Vector2(supportTexture.transform.localPosition.x, targetPosition);
     }
 
     /// <summary>
