f5c738956ae5b232601b119bed8fc30c9363b01b
Art126 <artalex126@gmail.com>
2025-01-19 15:35:30 -0500

    Particles for Geb's Earthquake
    Added particle effect to Geb's earthquake attack. This means that the earthquake damage zone is no longer invisible. The particles should be adjusted at some point to be less random and more spread out, so that way the edges of the damage zone are more defined.
    The particle effect is the same as the BreakParticles because I copied it and I don't know how to modify it the way I want.

--- Assets/Scripts/Entities/Bosses/Geb/GebEarthquakeZone.cs
+++ Assets/Scripts/Entities/Bosses/Geb/GebEarthquakeZone.cs
@@ -3,13 +3,22 @@ using System.Linq;
 using UnityEngine;
 
 // Copied & modified from EarthquakeZone.
+// \todo Update documentation for this file.
+// \todo Make the earthquake zone more visible (by making the particle spawn radius less random and more spread out).
 public class GebEarthquakeZone : MonoBehaviour
 {
-    [SerializeField] int damageAmount = 10;
-    [SerializeField] float timeBetweenDamage = .2f;
-    [SerializeField] float playerMoveVelocity = 6f;
-    float damageTimer = 0f;
+    [SerializeField] GameObject particleEffect;
 
+    [SerializeField] float particleSpawnRadius = 12f;
+    [SerializeField] float timeBetweenParticles = 0.2f;
+    [SerializeField] int damageAmount = 10;
+    [SerializeField] float timeBetweenDamage = 0.2f;
+    [SerializeField] float playerMoveVelocity = 6f;
+
+    /// Create random number generator.
+    private System.Random rng = new System.Random();
+    float particleTimer = 0f;
+    float damageTimer = 0f;
     List<PlayerHealth> objectsToDamage = new();
     float initialPlayerMoveVelocity;
 
@@ -20,23 +29,32 @@ public class GebEarthquakeZone : MonoBehaviour
 
     void Update()
     {
-        damageTimer += Time.deltaTime;
+        particleTimer += Time.deltaTime;
+        if (particleTimer > timeBetweenParticles)
+        {
+            particleTimer = 0;
 
+            Vector3 spawnPos = transform.position + new Vector3(((float)rng.NextDouble() * 2f - 1f) * particleSpawnRadius, 0f, 0f);
+            Instantiate(particleEffect, spawnPos, Quaternion.identity);
+        }
+
+        damageTimer += Time.deltaTime;
         if (damageTimer > timeBetweenDamage)
         {
+            damageTimer = 0;
+
             foreach (PlayerHealth health in objectsToDamage)
             {
                 health.TakeDamage(this.transform, damageAmount);
                 if (health.IsDead)
                     objectsToDamage.Remove(health);
             }
-
-            damageTimer = 0;
         }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // Reduce the player's move velocity when they enter the earthquake zone.
         if (col.TryGetComponent<PlayerMovement>(out var controller))
         {
             controller.moveVelocity = playerMoveVelocity;
@@ -50,6 +68,7 @@ public class GebEarthquakeZone : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D col)
     {
+        // Restore the player's move velocity when the exit the earthquake zone.
         if (col.TryGetComponent<PlayerMovement>(out var controller))
         {
             controller.moveVelocity = initialPlayerMoveVelocity;
