57c5b325527b82a5b43920dc773e218fdda9206d
Art126 <artalex126@gmail.com>
2025-03-09 20:28:49 -0400

    Breakable pot improvements
    - The breakable pot animation has been updated to work seamlessly with pots of any size and rotation.
    - The shards have been given soft colliders so they can be pushed around.
    - The pot shard hitboxes have been replaced with box colliders instead of polygon colliders for better performance.

--- Assets/Scripts/Entities/Base Components/SpawnPrefab.cs
+++ Assets/Scripts/Entities/Base Components/SpawnPrefab.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 Spawns a prefab. This function can be triggered by a Unity Event.
 This was initially made to drop potions when pots break.
 
-Documentation updated 3/5/2024
-\author Stephen Nuttall
+Documentation updated 3/9/2024
+\author Stephen Nuttall, Alexander Art
 */
 public class SpawnPrefab : MonoBehaviour
 {
     /// Reference to the prefab we want to spawn.
     [SerializeField] Transform prefabToSpawn;
+    /// True if the spawned prefab should spawn with the same scale as the object that runs this script.
+    [SerializeField] bool inheritScale;
+    /// True if the spawned prefab should spawn with the same rotation as the object that runs this script.
+    [SerializeField] bool inheritRotation;
 
-    /// Spawns the prefab at the parent object's position.
+    /// Spawns the prefab.
     public void Spawn()
     {
-        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        /// Spawns the prefab at the parent object's position.
+        Transform SpawnedPrefab = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+
+        // Set the scale of the spawned object if inheritRotation is true.
+        if (inheritRotation == true)
+        {
+            SpawnedPrefab.rotation = transform.rotation;
+        }
+
+        // Set the scale of the spawned object if inheritScale is true.
+        if (inheritScale == true)
+        {
+            SpawnedPrefab.localScale = transform.localScale;
+        }
     }
 }
