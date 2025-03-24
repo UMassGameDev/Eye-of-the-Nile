e2ad50271f84928b13f35d758410ec5e4569d669
Art126 <artalex126@gmail.com>
2025-03-23 22:27:24 -0400

    Added door locking mechanism
    The door objects can be locked and/or unlocked.
    I implemented this feature to the end of the current level.

--- Assets/Scripts/Hazards/DoorObject.cs
+++ Assets/Scripts/Hazards/DoorObject.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 /** \brief
 This script has functions for opening and closing the door object that this script is attached to.
+The door can be locked. The reason why a number is used for the doorLocked variable instead of a Boolean is, for example, if
+two separate events need to contribute toward unlocking the door, the door lock value can be incremented by 1/2 of the total,
+so the door won't get unlocked until both events happen.
 
-Documentation updated 2/12/2025
+Documentation updated 3/23/2025
 \author Alexander Art
 \todo (Maybe) Stop the player from being able to close the door on themself or other entities.
 */
@@ -18,6 +21,8 @@ public class DoorObject : MonoBehaviour
 
     /// Whether the door should be open or closed when the scene is loaded.
     [SerializeField] protected bool openByDefault = false;
+    /// If this number is greater than 0, then the door is locked.
+    [SerializeField] protected float doorLocked = 0f;
 
     /// The current state of the door
     private bool doorOpen = false;
@@ -26,13 +31,29 @@ public class DoorObject : MonoBehaviour
     {
         // Use openByDefault.
         if (openByDefault)
-            OpenDoor();
+            ForceOpenDoor();
         else
-            CloseDoor();
+            ForceCloseDoor();
     }
 
-    /// Method for opening the door.
+    /// Open the door, unless locked.
     public void OpenDoor()
+    {
+        if (doorLocked <= 0)
+        {
+            // Make the door appear open.
+            GetComponent<SpriteRenderer>().sprite = openSprite;
+
+            // Allow entities to pass through the door.
+            doorCollider.SetActive(false);
+
+            // Update the doorOpen variable.
+            doorOpen = true;
+        }
+    }
+
+    /// Open the door, even if locked.
+    public void ForceOpenDoor()
     {
         // Make the door appear open.
         GetComponent<SpriteRenderer>().sprite = openSprite;
@@ -44,8 +65,24 @@ public class DoorObject : MonoBehaviour
         doorOpen = true;
     }
 
-    /// Method for closing the door.
+    /// Close the door, unless locked.
     public void CloseDoor()
+    {
+        if (doorLocked <= 0)
+        {
+            // Make the door appear closed.
+            GetComponent<SpriteRenderer>().sprite = closedSprite;
+
+            // Stop entities from passing through the door.
+            doorCollider.SetActive(true);
+
+            // Update the doorOpen variable.
+            doorOpen = false;
+        }
+    }
+
+    /// Close the door, even if locked.
+    public void ForceCloseDoor()
     {
         // Make the door appear closed.
         GetComponent<SpriteRenderer>().sprite = closedSprite;
@@ -57,7 +94,7 @@ public class DoorObject : MonoBehaviour
         doorOpen = false;
     }
 
-    /// Method for opening/closing the door: closes the door if open, opens the door if closed.
+    /// Method for opening/closing the door: closes the door if open, opens the door if closed, unless locked.
     public void ToggleDoor()
     {
         if (doorOpen)
@@ -66,6 +103,24 @@ public class DoorObject : MonoBehaviour
             OpenDoor();
     }
 
-    /// Returns the current state of the door.
+    /// Alternative method for opening/closing the door: closes the door if open, opens the door if closed, even if locked.
+    public void ForceToggleDoor()
+    {
+        if (doorOpen)
+            ForceCloseDoor();
+        else
+            ForceOpenDoor();
+    }
+
+    /// Use this function to set the value of the lock. Anything greater than 0, and the door is locked.
+    public void SetLock(float value) { doorLocked = value; }
+
+    /// Use this function to increment the value of the lock.
+    public void ChangeLock(float value) { doorLocked += value; }
+
+    /// Returns true if the door is locked, otherwise false.
+    public bool GetLocked() { return doorLocked > 0; }
+
+    /// Returns the current open state of the door.
     public bool GetOpen() { return doorOpen; }
 }
