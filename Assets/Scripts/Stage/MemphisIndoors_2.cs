29c922a95ba5fa5478871aba30092eed9ddde1d0
Art126 <artalex126@gmail.com>
2025-03-28 23:56:35 -0400

    Improved continuity on MemphisIndoors_2 scene load
    When the player entered MemphisIndoors_2 from the exit (either by playing the level backwards or by turning around after entering the next scene), the sun shrine would be in an awkward/impossible spot. Now, when the player enters the scene from the back entrance, the sun shrine starts from where it usually ends, which fixes the continuity of the scene.

--- Assets/Scripts/Stage/MemphisIndoors_2.cs
+++ Assets/Scripts/Stage/MemphisIndoors_2.cs
@@ -9,7 +9,11 @@ Documentation updated 3/25/2025
 public class MemphisIndoors_2 : MonoBehaviour
 {
     /// Reference to the sun shrine's Rigidbody2D component.
-    [SerializeField] protected new Rigidbody2D rigidbody;
+    [SerializeField] protected Rigidbody2D rb;
+    /// Reference to the player's transform. Used for checking where the player is.
+    [SerializeField] protected Transform player;
+    /// Reference to the destructible wall. Gets removed immediately if the player enters from the StageWarp on the right (the exit).
+    [SerializeField] protected GameObject destructibleWall;
 
     /// The sun shrine's collision is an illusion. The statue only stops falling once this distance has been reached.
     [SerializeField] protected float maxFallDistance = 23f;
@@ -23,8 +27,8 @@ public class MemphisIndoors_2 : MonoBehaviour
     void Start()
     {
         initialHeight = transform.position.y;
-        initialGravityScale = rigidbody.gravityScale;
-        rigidbody.gravityScale = 0f;
+        initialGravityScale = rb.gravityScale;
+        rb.gravityScale = 0f;
     }
 
     void Update()
@@ -32,8 +36,16 @@ public class MemphisIndoors_2 : MonoBehaviour
         /// Limit how far the sun shrine can fall.
         if (transform.position.y <= initialHeight - maxFallDistance)
         {
-            rigidbody.gravityScale = 0f;
-            rigidbody.velocity = new Vector2(0f, 0f);
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(0f, 0f);
+            transform.position = new Vector2(transform.position.x, initialHeight - maxFallDistance);
+        }
+
+        // If the player enters the scene from the exit door (the rightmost StageWarp), then the statue is already fallen.
+        // This is in Update() because the player is not at the EntryPoint on Start().
+        if (1.5f < player.position.x && player.position.x < 13f && 14.5f < player.position.y && player.position.y < 20.5)
+        {
+            GameObject.Destroy(destructibleWall);
             transform.position = new Vector2(transform.position.x, initialHeight - maxFallDistance);
         }
     }
@@ -41,6 +53,6 @@ public class MemphisIndoors_2 : MonoBehaviour
     /// Used by InvokeOnInteract on the sun shrine to activate its gravity once the player interacts with it.
     public void StartFalling()
     {
-        rigidbody.gravityScale = initialGravityScale;
+        rb.gravityScale = initialGravityScale;
     }
 }
